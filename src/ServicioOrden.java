e7db910798740aa574c56f5e41cc82d0db5d1e76
Sebastian Mantilla Rodriguez <mantillarosebastian@gmail.com>
2026-05-23 12:10:37 -0500

    el punto 5 ServicioOrden coordina calculos, pagos y notificaciones automaticas mediante observadores validando todo correctamente desde Main.

---
+++ src/ServicioOrden.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+public class ServicioOrden implements Sujeto{
+    private ArrayList<ObservadorOrden> observadores;
+    private EstrategiaDescuento estrategiaDescuento;
+    private String mensajeEstado;
+    public ServicioOrden() {
+        observadores =new ArrayList<ObservadorOrden>();
+        this.estrategiaDescuento=new SinDescuentoEstrategia();
+    }
+    public void setEstrategiaDescuento (EstrategiaDescuento estrategiaDescuento){
+        this.estrategiaDescuento= estrategiaDescuento;
+    }
+    @Override
+    public void registrarObservador(ObservadorOrden o){
+        observadores.add(o);
+    }
+      @Override
+    public void removeObservador (ObservadorOrden o){
+        int i = observadores.indexOf(o):
+          if (i>= 0){
+              observadores.remove(i);
+          }
+      }
+    @Override
+    public void notificarObservadores() {
+        // Bucle clásico de recorrido secuencial con cast explícito (Fidelidad a la diapositiva)
+        for (int i = 0; i < observadores.size(); i++) {
+            ObservadorOrden observador = (ObservadorOrden) observadores.get(i);
+            observador.actualizar(mensajeEstado);
+        }
+    }
+    public void confirmarOrden (Carrito carrito, ProcesadorPago procesadorPago){
+        double subtotal = carrito.calcularTotal()();
+        double totalConDescuento  estrategiaDescuento.aplicarDescuento(subtotal);
+
+        procesadorPago.pagar(totalConDescuento);
+
+        this.mensajeEstado="Orden Procesada Exitosamente";
+        notificarObservadores();
+    }
+
+}
