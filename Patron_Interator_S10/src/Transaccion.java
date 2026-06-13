6ef5d86730c0c18cad0dc4e73ef039bd4c88717d
Sebastian Mantilla Rodriguez <mantillarosebastian@gmail.com>
2026-06-13 18:40:49 -0500

    en esta clase solo almacenamos 4 suficiente para la prueba que podremos usar luego para tener un codigo, la hora que se hizo, el tipo de operacion y el monto de dinero

--- Patron_Interator_S10/src/Transaccion.java
+++ Patron_Interator_S10/src/Transaccion.java
@@ -1,4 +1,26 @@
 public class Transaccion {
-    prueba22
+    private String codigo;
+    private String hora;
+    private String tipo;
+    private double monto;
+
+    public Transaccion(String codigo, String hora, String tipo, double monto ){
+        this.codigo =codigo;
+        this.hora = hora;
+        this.tipo = tipo;
+        this.monto = monto;
+    }
+    public String getCodigo(){
+        return codigo;
+    }
+    public String getHora(){
+        return hora;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public double getMonto(){
+        return monto;
+    }
 
 }
